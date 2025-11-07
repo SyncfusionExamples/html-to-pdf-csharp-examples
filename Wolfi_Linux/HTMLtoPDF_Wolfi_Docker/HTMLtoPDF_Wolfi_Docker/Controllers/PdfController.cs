12a20cd3fb907402e5488f5b2d2a140f30af4469
Irfana Jaffer Sadhik <irfana.jaffersadhik@syncfusion.com>
2025-11-07 16:09:37 +0530

    Task-985370-WolfiLinux Sample Committed

---
+++ Wolfi_Linux/HTMLtoPDF_Wolfi_Docker/HTMLtoPDF_Wolfi_Docker/Controllers/PdfController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Drawing;
+using Syncfusion.Pdf;
+
+namespace HTMLtoPDF_Wolfi_Docker.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+
+        public IActionResult ConvertPDF()
+        {
+            try
+            {
+                //Initialize the HTML to PDF converter.
+                HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+
+                BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+                blinkConverterSettings.CommandLineArguments.Add("--disable-gpu");
+                blinkConverterSettings.BlinkPath = "/usr/lib/chromium";
+                //Assign Blink converter settings to HTML converter
+                htmlConverter.ConverterSettings = blinkConverterSettings;
+
+                //Convert URL to PDF
+                PdfDocument document = htmlConverter.Convert("https://www.google.com");
+               MemoryStream fileStream = new MemoryStream();
+                //Save and close the PDF document.
+                document.Save(fileStream);
+                document.Close(true);
+                fileStream.Position = 0;
+                return new FileStreamResult(fileStream, "application/pdf") {FileDownloadName="HtmlToPdf_Output.pdf" };
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message.ToString());
+            }
+        }
+    }
+}
