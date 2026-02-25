b87992e32ffa13a078fb6654e1a27f83a2246e22
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-02-25 11:07:05 +0530

    1011736: Added sample project for HTML to PDF on the Windows Server using IIS Manager.

---
+++ Windows_Server/HTML_to_PDF_on_windows_server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTML_to_PDF_on_windows_server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
