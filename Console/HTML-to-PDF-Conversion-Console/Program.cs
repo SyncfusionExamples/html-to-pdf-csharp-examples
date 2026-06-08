d2c1ede5be7110ed70cd790e24360e41ea6187f5
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-06-08 12:37:50 +0530

    1031908: Added Getting Started: Console App

---
+++ Console/HTML-to-PDF-Conversion-Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+
+//Initialize HTML to PDF converter.
+HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+//Convert URL to PDF
+using (PdfDocument document = htmlConverter.Convert("https://www.google.com"))
+{
+    //Save the PDF document.
+    document.Save(Path.GetFullPath(@"Output/Output.pdf"));
+}
