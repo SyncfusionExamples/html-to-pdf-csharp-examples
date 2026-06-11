f5fcf09e60c4e226ebeeb4c2d32951f2a3d04394
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-06-11 11:28:04 +0530

    1031908: Added proper link.

--- Console/HTML-to-PDF-Conversion-Console/Program.cs
+++ Console/HTML-to-PDF-Conversion-Console/Program.cs
@@ -9,7 +9,7 @@ blinkConverterSettings.ViewPortSize = new Syncfusion.Drawing.Size(1280, 0);
 //Assign Blink converter settings to HTML converter.
 htmlConverter.ConverterSettings = blinkConverterSettings;
 //Convert URL to PDF document.
-using (PdfDocument document = htmlConverter.Convert("https://www.google.com"))
+using (PdfDocument document = htmlConverter.Convert("https://www.Syncfusion.com"))
 {
     //Save the PDF document.
     document.Save(Path.GetFullPath(@"Output/Output.pdf"));
