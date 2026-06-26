79a577647c2fdbd58928f6d2c1ebf164023b2dbc
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-06-26 16:30:17 +0530

    1035366: Resolved given feedback.

--- Web API/Web-API-Project/Controllers/PdfController.cs
+++ Web API/Web-API-Project/Controllers/PdfController.cs
@@ -9,10 +9,6 @@ namespace Web_API_Project.Controllers
     [Route("[controller]")]
     public class PdfController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         private readonly ILogger<PdfController> _logger;
 
@@ -24,34 +20,11 @@ namespace Web_API_Project.Controllers
         [HttpGet("/api/Pdf")]
         public IActionResult ConvertHTMLtoPDF()
         {
-            try
-            {
-                const string fileDownloadName = "Output.pdf";
-                const string contentType = "application/pdf";
-                var stream = ExportWeatherForecastToPdf();
-                stream.Position = 0;
-                return File(stream, contentType, fileDownloadName);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"Error occurred while creating PDF file: {ex.Message}");
-            }
-        }
-
-        private MemoryStream ExportWeatherForecastToPdf()
-        {
-            var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToList();
-
             //Initialize HTML to PDF converter.
             HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
             BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
             //Set Blink viewport size.
-            blinkConverterSettings.ViewPortSize =new Size(1280, 0);
+            blinkConverterSettings.ViewPortSize = new Size(1280, 0);
             //Assign Blink converter settings to HTML converter.
             htmlConverter.ConverterSettings = blinkConverterSettings;
             //Convert URL to PDF document.
@@ -60,7 +33,8 @@ namespace Web_API_Project.Controllers
             MemoryStream stream = new MemoryStream();
             //Save the document to memory stream.
             document.Save(stream);
-            return new MemoryStream(stream.ToArray());
+            stream.Position = 0;
+            return File(stream, "application/pdf", "Output.pdf");
         }
     }
 }
